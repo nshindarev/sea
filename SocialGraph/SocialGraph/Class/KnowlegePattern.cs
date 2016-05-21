3a8688a0698db6b19597fad638ae3c66fe609705
Shindarev Nikita <nshindarev@gmail.com>
2016-05-22 00:50:18 +0300

    Created generation of structure

--- SocialGraph/SocialGraph/Class/KnowlegePattern.cs
+++ SocialGraph/SocialGraph/Class/KnowlegePattern.cs
@@ -6,7 +6,7 @@ namespace SocialGraph
 {
 	public class KnowlegePattern
 	{
-		public List<List<Dictionary<string,double>>> Data;
+		public List<Dictionary<string,double>> Data;
 		public Dictionary<Edge<string>, double> edgeWeight { get; set;}
 		public Dictionary<string, double> vertexWeight { get; set;}
 		public InputGraph G;
@@ -29,7 +29,7 @@ namespace SocialGraph
 			
 			// сначала добавляем одиночные вершины
 			// <<n>> списков одиночных списков из 1го словаря: "("user1_user1", p(user1))"
-			List<Dictionary<string,double>> first_floor = new List<Dictionary<string, double>>(staff.Count);
+			/*List<Dictionary<string,double>> first_floor = new List<Dictionary<string, double>>(staff.Count);
 			for (int i = 0; i < staff.Count; i++) {
 				// создаем 1 список одиночных вершин
 				Dictionary<string,double> cur = new Dictionary<string,double> ();
@@ -41,7 +41,7 @@ namespace SocialGraph
 
 			/* Теперь генерируем для каждого уровня "n" число списков = числу выборки "C_n^k"
 			 * k - это текущий уровень
-			 */
+			 
 			if(staff.Count > 2){
 
 			
@@ -56,19 +56,40 @@ namespace SocialGraph
 					}
 				}
 			}
-
+			*/
 			int fragmentlength = 0;
 			for (int i = 1; i < staff.Count; i++)
 				fragmentlength += Model.C_nk (staff.Count, i);
+	
 
+			 
 			// 1) Сопоставляем двоичному коду индекса списка выборку.
-			// 2) Для выборки генерируем перестановки (см. Java)
+			// 2) Для выборки генерируем cловарь перестановок. p["start finish"] = ... (см. Java)
 			// TODO: надо расширить функционал Java кода с цифр на пользователей И.С. 
+			this.Data = new List<Dictionary<string, double>> (fragmentlength);
 			for (int i = 1; i < fragmentlength; i++) {
-				List<string> check = Model.generate_selection (i, staff);
-				for (int j = 0; j < check.Count; j++) {
- 					
+				List<string> choose_at_number = Model.generate_selection (i, staff);
+				Dictionary<string,double> permutation_list = new Dictionary<string,double> (Model.math_factorial(choose_at_number.Count));
+				for (int j = 0; j < Model.math_factorial(choose_at_number.Count); j++) {
+					List<string> cur_permutation = Model.get_sort_bynumber (j, choose_at_number);
+					for (int k = 0; k < cur_permutation.Count; k++) {
+						double probability = 0;
+						// стартовая вершина
+						if (k == 0)
+							probability = vertexWeight [cur_permutation [k]];
+						// нестартовая - исп. дуги
+						else {
+							Edge<string> e;
+							string start = cur_permutation [k - 1];
+							string finish = cur_permutation [k];
+							if (G.social_graph.TryGetEdge (start, finish,out e))
+								probability *= G.edgeWeight [e];
+						}
+						string st_fin = cur_permutation [0] + "__" + cur_permutation [cur_permutation.Count - 1] + "__" + k.ToString();
+						permutation_list.Add (st_fin, probability);
+					}
 				}
+				Data.Add (permutation_list);
 			}
 		}
 		/*public string[] getusersbynumber (int num){ return 0;
