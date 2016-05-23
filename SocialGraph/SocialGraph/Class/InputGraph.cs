40f933f1f4d0052c358371fac0ee11d37c34b876
Shindarev Nikita <nshindarev@gmail.com>
2016-05-23 16:45:02 +0300

    Alpha version

--- SocialGraph/SocialGraph/Class/InputGraph.cs
+++ SocialGraph/SocialGraph/Class/InputGraph.cs
@@ -13,25 +13,39 @@ namespace SocialGraph
 
 		public InputGraph (string input){
 			// определяем нужные объекты
-			var reader   = new StreamReader (input);
-			var social_graph  = new AdjacencyGraph<string, Edge<string>> ();
+
+
+
+			StreamReader reader   = new StreamReader (input);
+			this.social_graph  = new AdjacencyGraph<string, Edge<string>> ();
 
 			// словари, хранящие веса ребер и вершин
-			var edgeWeight   = new Dictionary<Edge<string>, double> ();
-			var vertexWeight = new Dictionary<string, double> ();
+			this.edgeWeight   = new Dictionary<Edge<string>, double> ();
+			this.vertexWeight = new Dictionary<string, double> ();
 
+			//for(int i = 0; i< departmentstxt.Count; i++)
+				//department[i] = 
 			// считываем взвешенный граф из файла
 			// структура input.txt: start || finish || weight_edge || weight_start || weight_finish
+
+
 			while (!reader.EndOfStream){
+
+
+
 				string[] lines = reader.ReadLine ().Split (' ');
-				var edge = new Edge<string> (lines [0], lines [1]);
-				social_graph.AddVerticesAndEdge (edge);
-				edgeWeight.Add (edge, Convert.ToDouble (lines [2]));
+				Edge<string> edge = new Edge<string> (lines [0], lines [1]);
+				this.social_graph.AddVerticesAndEdge (edge);
+				this.edgeWeight.Add (edge, Convert.ToDouble (lines [2]));
+
 
 				// если строка содержит информацию о весах вершин
 				if (lines.Length == 5) {
-					vertexWeight.Add (lines[0], Convert.ToDouble(lines[3]));
-					vertexWeight.Add (lines[1], Convert.ToDouble(lines[4]));
+					if (!vertexWeight.ContainsKey(lines[0]))
+					this.vertexWeight.Add (lines[0], Convert.ToDouble(lines[2]));
+
+					if (!vertexWeight.ContainsKey(lines[1]))
+					this.vertexWeight.Add (lines[1], Convert.ToDouble(lines[3]));
 				}
 			}
 		}
